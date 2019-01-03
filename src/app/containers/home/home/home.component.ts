054ab61784e305168565368d56fe6befcceadbd9
Ivan Anchev <ivan.anchev23@gmail.com>
2019-01-03 12:12:06 +0200

    style: changed active state of avatar on home menu

--- src/app/containers/home/home/home.component.ts
+++ src/app/containers/home/home/home.component.ts
@@ -15,12 +15,24 @@ import { selectIsConnected, selectIsSessionChecked, selectUser, selectIsProfileE
 })
 export class HomeComponent implements OnInit {
 
+  /**
+   * Is user connected to WS
+   */
   isConnected$: Observable<boolean>;
 
+  /**
+   * Is session check ready
+   */
   isSessionCheckReady$: Observable<boolean>;
 
+  /**
+   * Is UI in profile edit mode
+   */
   isProfileEditMode$: Observable<boolean>;
 
+  /**
+   * Current user object
+   */
   user$: Observable<User>;
 
   constructor(private _store: Store<AppState>) {
@@ -30,12 +42,20 @@ export class HomeComponent implements OnInit {
     this.isProfileEditMode$ = this._store.select(selectIsProfileEditMode);
   }
 
+  /**
+   * Set profile edit mode for UI
+   * @param  isProfileEditMode boolean
+   */
   setProfileEditMode(isProfileEditMode: boolean) {
     this._store.dispatch(new SetProfileEditMode({ isProfileEditMode }));
   }
 
-  onProfileSubmit(userData: User) {
-    this._store.dispatch(new SetUser(userData));
+  /**
+   * Set current user data after edit
+   * @param  userData User
+   */
+  onProfileSubmit({ name, avatar }: User) {
+    this._store.dispatch(new SetUser({ name, avatar }));
     this._store.dispatch(new SetProfileEditMode({ isProfileEditMode: false }));
   }
 
