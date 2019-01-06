89390cade976beda5302658ad1e9c31f9cca53e8
Ivan Anchev <ivan.anchev23@gmail.com>
2019-01-06 16:17:51 +0200

    refactor: renamed app component selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'fiar-root',
+  selector: 'fiar-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
